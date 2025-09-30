4bf00b4d1037d2302fe3ab4e75fea225b1a6ba9b
OzerBerkay <berkay1690@gmail.com>
2025-09-30 19:38:45 +0300

    Spring boot starter class and config added

---
+++ order-service/order-container/src/main/java/com/berkay/order/service/application/OrderServiceApplication.java
@@ -0,0 +1,15 @@
+package com.berkay.order.service.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = { "com.berkay.order.service.dataaccess"})
+@EntityScan(basePackages = { "com.food.ordering.system.order.service.dataaccess"})
+@SpringBootApplication(scanBasePackages = "com.berkay")
+public class OrderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+}
