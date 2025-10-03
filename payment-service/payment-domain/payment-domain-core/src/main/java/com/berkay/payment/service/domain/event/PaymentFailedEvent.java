7607cd48c627f2da046e8e1ab213b791d86821a3
OzerBerkay <berkay1690@gmail.com>
2025-10-03 04:04:11 +0300

    Exceptions and events created for payment service domain core

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/berkay/payment/service/domain/event/PaymentFailedEvent.java
@@ -0,0 +1,16 @@
+package com.berkay.payment.service.domain.event;
+
+import com.berkay.payment.service.domain.entity.Payment;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class PaymentFailedEvent extends PaymentEvent {
+
+    public PaymentFailedEvent(Payment payment,
+                              ZonedDateTime createdAt,
+                              List<String> failureMessages) {
+        super(payment, createdAt, failureMessages);
+    }
+
+}
---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/berkay/payment/service/domain/exception/PaymentDomainException.java
@@ -0,0 +1,13 @@
+package com.berkay.payment.service.domain.exception;
+
+import com.berkay.domain.exception.DomainException;
+
+public class PaymentDomainException extends DomainException {
+    public PaymentDomainException(String message) {
+        super(message);
+    }
+
+    public PaymentDomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
