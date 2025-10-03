1124346fa43668c0a6909dd9ec820313b7b8e81c
OzerBerkay <berkay1690@gmail.com>
2025-10-04 01:32:00 +0300

    Application service domain module completed

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/berkay/payment/service/domain/ports/output/repository/CreditEntryRepository.java
@@ -0,0 +1,13 @@
+package com.berkay.payment.service.domain.ports.output.repository;
+
+import com.berkay.domain.valueobject.CustomerId;
+import com.berkay.payment.service.domain.entity.CreditEntry;
+
+import java.util.Optional;
+
+public interface CreditEntryRepository {
+
+    CreditEntry save(CreditEntry creditEntry);
+
+    Optional<CreditEntry> findByCustomerId(CustomerId customerId);
+}
