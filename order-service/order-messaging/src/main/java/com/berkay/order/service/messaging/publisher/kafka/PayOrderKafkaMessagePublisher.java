c96d830c2b79d01c72692e59d42058d3a81d8104
OzerBerkay <berkay1690@gmail.com>
2025-10-06 00:41:13 +0300

    Publishers, mapper added into messaging module and kafka message helper made generic for all microservices' publishers

--- order-service/order-messaging/src/main/java/com/berkay/order/service/messaging/publisher/kafka/PayOrderKafkaMessagePublisher.java
+++ order-service/order-messaging/src/main/java/com/berkay/order/service/messaging/publisher/kafka/PayOrderKafkaMessagePublisher.java
@@ -1,6 +1,7 @@
 package com.berkay.order.service.messaging.publisher.kafka;
 
 import com.berkay.kafka.order.avro.model.RestaurantApprovalRequestAvroModel;
+import com.berkay.kafka.producer.KafkaMessageHelper;
 import com.berkay.kafka.producer.service.KafkaProducer;
 import com.berkay.order.service.domain.config.OrderServiceConfigData;
 import com.berkay.order.service.domain.event.OrderPaidEvent;
@@ -16,9 +17,9 @@ public class PayOrderKafkaMessagePublisher implements OrderPaidRestaurantRequest
     private final OrderMessagingDataMapper orderMessagingDataMapper;
     private final OrderServiceConfigData orderServiceConfigData;
     private final KafkaProducer<String, RestaurantApprovalRequestAvroModel> kafkaProducer;
-    private final OrderKafkaMessageHelper orderKafkaMessageHelper;
+    private final KafkaMessageHelper orderKafkaMessageHelper;
 
-    public PayOrderKafkaMessagePublisher(OrderMessagingDataMapper orderMessagingDataMapper, OrderServiceConfigData orderServiceConfigData, KafkaProducer<String, RestaurantApprovalRequestAvroModel> kafkaProducer, OrderKafkaMessageHelper orderKafkaMessageHelper) {
+    public PayOrderKafkaMessagePublisher(OrderMessagingDataMapper orderMessagingDataMapper, OrderServiceConfigData orderServiceConfigData, KafkaProducer<String, RestaurantApprovalRequestAvroModel> kafkaProducer, KafkaMessageHelper orderKafkaMessageHelper) {
         this.orderMessagingDataMapper = orderMessagingDataMapper;
         this.orderServiceConfigData = orderServiceConfigData;
         this.kafkaProducer = kafkaProducer;
