b63e08343ed9cbf3e9f07c5b9f1b9a8a85f66459
OzerBerkay <berkay1690@gmail.com>
2025-10-09 07:39:07 +0300

    Container module completed for restaurant service

---
+++ restaurant-service/restaurant-container/src/main/java/com/berkay/restaurant/service/domain/RestaurantServiceApplication.java
@@ -0,0 +1,15 @@
+package com.berkay.restaurant.service.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = { "com.berkay.restaurant.service.dataaccess", "com.berkay.dataaccess" })
+@EntityScan(basePackages = { "com.berkay.restaurant.service.dataaccess", "com.berkay.dataaccess" })
+@SpringBootApplication(scanBasePackages = "com.berkay")
+public class RestaurantServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantServiceApplication.class, args);
+    }
+}
