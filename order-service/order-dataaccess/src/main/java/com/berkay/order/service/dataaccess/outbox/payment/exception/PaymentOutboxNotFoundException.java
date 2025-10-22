fb2ef620830cd7a585ac501abdda1998c7ad4774
OzerBerkay <berkay1690@gmail.com>
2025-10-22 12:41:10 +0300

    repository part completed for outbox in order data access

---
+++ order-service/order-dataaccess/src/main/java/com/berkay/order/service/dataaccess/outbox/payment/exception/PaymentOutboxNotFoundException.java
@@ -0,0 +1,8 @@
+package com.berkay.order.service.dataaccess.outbox.payment.exception;
+
+public class PaymentOutboxNotFoundException extends RuntimeException {
+
+    public PaymentOutboxNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ order-service/order-dataaccess/src/main/java/com/berkay/order/service/dataaccess/outbox/restaurantapproval/repository/ApprovalOutboxJpaRepository.java
@@ -0,0 +1,28 @@
+package com.berkay.order.service.dataaccess.outbox.restaurantapproval.repository;
+
+import com.berkay.order.service.dataaccess.outbox.restaurantapproval.entity.ApprovalOutboxEntity;
+import com.berkay.outbox.OutboxStatus;
+import com.berkay.saga.SagaStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ApprovalOutboxJpaRepository extends JpaRepository<ApprovalOutboxEntity, UUID> {
+
+    Optional<List<ApprovalOutboxEntity>> findByTypeAndOutboxStatusAndSagaStatusIn(String type,
+                                                                                  OutboxStatus outboxStatus,
+                                                                                  List<SagaStatus> sagaStatus);
+
+    Optional<ApprovalOutboxEntity> findByTypeAndSagaIdAndSagaStatusIn(String type,
+                                                                      UUID sagaId,
+                                                                      List<SagaStatus> sagaStatus);
+
+    void deleteByTypeAndOutboxStatusAndSagaStatusIn(String type,
+                                                    OutboxStatus outboxStatus,
+                                                    List<SagaStatus> sagaStatus);
+
+}
