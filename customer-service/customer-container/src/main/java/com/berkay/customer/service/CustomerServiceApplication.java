e562b053282f311cd0d0997c2d15756c846a5acb
OzerBerkay <berkay1690@gmail.com>
2025-11-06 23:50:41 +0300

    Customer container module created

---
+++ customer-service/customer-container/src/main/java/com/berkay/customer/service/CustomerServiceApplication.java
@@ -0,0 +1,15 @@
+package com.berkay.customer.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = { "com.berkay.customer.service.dataaccess", "com.berkay.dataaccess"})
+@EntityScan(basePackages = { "com.berkay.customer.service.dataaccess", "com.berkay.dataaccess" })
+@SpringBootApplication(scanBasePackages = "com.berkay")
+public class CustomerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+}
