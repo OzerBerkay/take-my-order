1d6a70eb870062ae29ec48b3dd36de9ca182ee98
OzerBerkay <berkay1690@gmail.com>
2026-01-10 06:54:21 +0300

    product and restaurant entities added to data layer

---
+++ restaurant-service/restaurant-dataaccess/src/main/java/com/berkay/restaurant/service/dataaccess/restaurant/entity/ProductEntity.java
@@ -0,0 +1,43 @@
+package com.berkay.restaurant.service.dataaccess.restaurant.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+@Entity
+public class ProductEntity {
+
+    @Id
+    private UUID productId;
+
+    private String name;
+    private BigDecimal price;
+    private int stock;
+    private boolean available;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "restaurant_id")
+    private RestaurantEntity restaurant;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductEntity that = (ProductEntity) o;
+        return productId.equals(that.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId);
+    }
+}
