734b68fe4f485d31be81d80b67a593aad90eef8f
OzerBerkay <berkay1690@gmail.com>
2026-01-10 07:02:48 +0300

    Exception handlers added for restaurant service

---
+++ restaurant-service/restaurant-application/src/main/java/com/berkay/restaurant/service/domain/exception/handler/RestaurantGlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.berkay.restaurant.service.domain.exception.handler;
+
+import com.berkay.application.handler.GlobalExceptionHandler;
+import com.berkay.application.handler.ErrorDTO;
+import com.berkay.restaurant.service.domain.exception.RestaurantDomainException;
+import com.berkay.restaurant.service.domain.exception.RestaurantNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+@ControllerAdvice
+public class RestaurantGlobalExceptionHandler extends GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = {RestaurantDomainException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleException(RestaurantDomainException restaurantDomainException) {
+        log.error(restaurantDomainException.getMessage(), restaurantDomainException);
+        return ErrorDTO.builder()
+                .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message(restaurantDomainException.getMessage())
+                .build();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = {RestaurantNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorDTO handleException(RestaurantNotFoundException restaurantNotFoundException) {
+        log.error(restaurantNotFoundException.getMessage(), restaurantNotFoundException);
+        return ErrorDTO.builder()
+                .code(HttpStatus.NOT_FOUND.getReasonPhrase())
+                .message(restaurantNotFoundException.getMessage())
+                .build();
+    }
+}
