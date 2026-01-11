d6c4e728ca9687779668630179f403edd30ef6f1
OzerBerkay <berkay1690@gmail.com>
2026-01-11 15:41:35 +0300

    Remove common dataaccess module for unused Restaurant entities

--- common/common-dataaccess/src/main/java/com/berkay/dataaccess/restaurant/exception/RestaurantDataAccessException.java
+++
@@ -1,8 +0,0 @@
-package com.berkay.dataaccess.restaurant.exception;
-
-public class RestaurantDataAccessException extends RuntimeException{
-
-    public RestaurantDataAccessException(String message) {
-        super(message);
-    }
-}
