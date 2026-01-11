626738d1b4d561b1dbc8e9d41bdc758c94d2bc0b
OzerBerkay <berkay1690@gmail.com>
2026-01-11 17:27:57 +0300

    Cleaning up common-dataaccess remnants

--- order-service/order-container/src/main/java/com/berkay/order/service/application/OrderServiceApplication.java
+++ order-service/order-container/src/main/java/com/berkay/order/service/application/OrderServiceApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = { "com.berkay.order.service.dataaccess", "com.berkay.dataaccess"})
-@EntityScan(basePackages = { "com.berkay.order.service.dataaccess", "com.berkay.dataaccess"})
+@EnableJpaRepositories(basePackages = { "com.berkay.order.service.dataaccess"})
+@EntityScan(basePackages = { "com.berkay.order.service.dataaccess"})
 @SpringBootApplication(scanBasePackages = "com.berkay")
 public class OrderServiceApplication {
     public static void main(String[] args) {
--- payment-service/payment-container/src/main/java/com/berkay/payment/service/domain/PaymentServiceApplication.java
+++ payment-service/payment-container/src/main/java/com/berkay/payment/service/domain/PaymentServiceApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = { "com.berkay.payment.service.dataaccess", "com.berkay.dataaccess"})
-@EntityScan(basePackages = { "com.berkay.payment.service.dataaccess", "com.berkay.dataaccess"})
+@EnableJpaRepositories(basePackages = { "com.berkay.payment.service.dataaccess"})
+@EntityScan(basePackages = { "com.berkay.payment.service.dataaccess"})
 @SpringBootApplication(scanBasePackages = "com.berkay")
 public class PaymentServiceApplication {
     public static void main(String[] args) {
