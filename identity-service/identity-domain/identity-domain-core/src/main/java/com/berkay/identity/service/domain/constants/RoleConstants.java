3d9c08a41aa8ad407c4e45f1e38e1e4e0472f801
OzerBerkay <berkay1690@gmail.com>
2026-02-02 15:33:52 +0300

    Add missing RoleConstants class to resolve compilation errors

---
+++ identity-service/identity-domain/identity-domain-core/src/main/java/com/berkay/identity/service/domain/constants/RoleConstants.java
@@ -0,0 +1,7 @@
+package com.berkay.identity.service.domain.constants;
+
+public class RoleConstants {
+    public static final String ROLE_CUSTOMER = "ROLE_CUSTOMER";
+    public static final String ROLE_MERCHANT = "ROLE_MERCHANT";
+    public static final String ROLE_INTERNAL = "ROLE_MERCHANT";
+}
