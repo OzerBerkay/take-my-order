4850a2b55fc16982ea24925b9012dc0856c68294
Berkay Ozer <berkay1690@gmail.com>
2026-08-06 05:48:58 +0300

    refactor: delete deprecated customer service - data access & messaging

--- customer-service/customer-dataaccess/src/main/java/com/berkay/customer/service/dataaccess/outbox/exception/CustomerOutboxNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.berkay.customer.service.dataaccess.outbox.exception;
-
-public class CustomerOutboxNotFoundException extends RuntimeException {
-    public CustomerOutboxNotFoundException(String message) {}
-}
