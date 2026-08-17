76829c77519628fe4a67f7436122ff789e8626f0
Berkay Ozer <berkay1690@gmail.com>
2026-08-17 23:59:35 +0300

    refactor(payment): remove legacy credit system and corresponding handlers

--- payment-service/payment-dataaccess/src/main/java/com/berkay/payment/service/dataaccess/credithistory/exception/CreditHistoryDataaccessException.java
+++
@@ -1,8 +0,0 @@
-package com.berkay.payment.service.dataaccess.credithistory.exception;
-
-public class CreditHistoryDataaccessException extends RuntimeException {
-
-    public CreditHistoryDataaccessException(String message) {
-        super(message);
-    }
-}
