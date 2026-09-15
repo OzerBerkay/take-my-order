5b764c9fabad312ad842cf55c2c7dbd817db34f1
Berkay Ozer <berkay1690@gmail.com>
2026-09-15 23:04:08 +0300

    Remove unused import

--- identity-service/identity-application/src/main/java/com/berkay/identity/service/application/rest/PermissionController.java
+++ identity-service/identity-application/src/main/java/com/berkay/identity/service/application/rest/PermissionController.java
@@ -7,7 +7,6 @@ import com.berkay.identity.service.ports.input.service.PermissionApplicationServ
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
